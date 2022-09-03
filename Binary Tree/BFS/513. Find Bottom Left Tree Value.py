58c9051a4e864b42465079a509a543409c0e3c53
Priyanka Asnani <50499754+priyanka-asnani@users.noreply.github.com>
2022-09-03 08:57:02 -0700

    https://leetcode.com/problems/find-bottom-left-tree-value/

---
+++ Binary Tree/BFS/513. Find Bottom Left Tree Value.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return None
+        
+        q = deque([root])
+        while q:
+            num_nodes = len(q)
+            temp = []
+            for i in range(num_nodes):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                temp.append(node.val)
+        return temp[0]
+    
+## Time Complexity: O(N)
+## Space Complexity: O(N)
