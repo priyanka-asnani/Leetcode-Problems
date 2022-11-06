f44b1360c8c18e94ef923bdece662e216ec00ec8
Priyanka Asnani <50499754+priyanka-asnani@users.noreply.github.com>
2022-11-05 17:58:28 -0700

    https://leetcode.com/problems/peak-index-in-a-mountain-array/

--- Binary Search/852. Peak Index in a Mountain Array.py
+++ Binary Search/852. Peak Index in a Mountain Array.py
@@ -1,13 +1,16 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         left, right = 0, len(arr) - 1
-        while left < right:
+        index = -1
+        while left <= right:
             mid = left + (right - left) // 2
-            if arr[mid] <= arr[mid+1]:
-                left = mid + 1
+            if arr[mid + 1] < arr[mid]:
+                # record the peak index
+                index = mid
+                right = mid - 1
             else:
-                right = mid
-        return left
+                left = mid + 1
+        return index
 
 ## Time Complexity: O(log N)
 ## Space Complexity: O(1)
