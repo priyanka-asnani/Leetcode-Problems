692a27681d7bbd7f8c25d34e285a0c00b2b51065
Priyanka Asnani <50499754+priyanka-asnani@users.noreply.github.com>
2022-11-06 05:38:16 -0800

    https://leetcode.com/problems/find-peak-element/

--- Binary Search/162. Find Peak Element.py
+++ Binary Search/162. Find Peak Element.py
@@ -1,13 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
-        while left < right:
+        index = right
+        while left <= right:
             mid = left + (right - left) // 2
-            if nums[mid] <= nums[mid+1]:
-                left = mid + 1
+            if mid+1 < len(nums) and nums[mid+1] < nums[mid]:
+                # record the peak index
+                index = mid
+                right = mid - 1
             else:
-                right = mid
-        return left
+                left = mid + 1
+        return index
+
 
 ## Time Complexity: O(log N)
 ## Space Complexity: O(1)
